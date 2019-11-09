fb3028ae7ca9ab4cb17e20a340852cf1988f1003
Aditya Chandra <inibukanadit@gmail.com>
2019-11-10 03:07:22 +0700

    complete scaf1-appbar

--- lib/main.dart
+++ lib/main.dart
@@ -10,17 +10,39 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
+
+  int _value = 0;
+
+  void _add() {
+    setState(() {
+      _value++;
+    });
+  }
+
+  void _remove() {
+    setState(() {
+      _value--;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Name Here'),
+        backgroundColor: Colors.red,
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.add), onPressed: _add),
+          IconButton(icon: Icon(Icons.remove), onPressed: _remove)
+        ],
       ),
       body: Container(
         padding: EdgeInsets.all(32),
         child: Center(
           child: Column(
-            children: <Widget>[Text('Add Widgets here')],
+            children: <Widget>[
+              Text(_value.toString(), style: TextStyle(fontWeight: FontWeight.bold, fontSize: 37),)
+            ],
           ),
         ),
       ),
