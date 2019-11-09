d40c159277c58aad75443cc669ed520cbf9b3035
Aditya Chandra <inibukanadit@gmail.com>
2019-11-09 22:03:16 +0700

    initial blank project

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home: MyApp()));
+
+class MyApp extends StatefulWidget {
+  @override
+  _State createState() {
+    return _State();
+  }
+}
+
+class _State extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Name Here'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(32),
+        child: Center(
+          child: Column(
+            children: <Widget>[Text('Add Widgets here')],
+          ),
+        ),
+      ),
+    );
+  }
+}
