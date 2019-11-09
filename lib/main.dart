4a19d19ec422d511674ead6cc4d269255954930f
Aditya Chandra <inibukanadit@gmail.com>
2019-11-09 22:18:40 +0700

    complete input1

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,20 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
+  String _value = '';
+
+  void _onChanged(String value) {
+    setState(() {
+      _value = 'On Change : $value';
+    });
+  }
+
+  void _onSubmitted(String value) {
+    setState(() {
+      _value = 'On Submit : $value';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +34,20 @@ class _State extends State<MyApp> {
         padding: EdgeInsets.all(32),
         child: Center(
           child: Column(
-            children: <Widget>[Text('Add Widgets here')],
+            children: <Widget>[
+              Text(_value),
+              TextField(
+                decoration: InputDecoration(
+                    icon: Icon(Icons.people),
+                    labelText: 'Email',
+                    hintText: 'johndoe@gmail.com'),
+                autocorrect: true,
+                autofocus: true,
+                keyboardType: TextInputType.emailAddress,
+                onChanged: _onChanged,
+                onSubmitted: _onSubmitted,
+              )
+            ],
           ),
         ),
       ),
