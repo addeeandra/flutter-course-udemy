30927dc0bcacdcfc046c544e550b936fe34f0150
Aditya Chandra <inibukanadit@gmail.com>
2019-11-10 03:15:34 +0700

    complete scaf3

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,39 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
+
+  bool _value = false;
+
+  void _onValueChanged(bool value) {
+    setState(() {
+      _value = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Name Here'),
       ),
+      drawer: Drawer(
+          child: Container(
+        padding: EdgeInsets.all(32),
+        child: Column(
+          children: <Widget>[
+            CheckboxListTile(
+              onChanged: _onValueChanged,
+              value: _value,
+              title: Text('Hello Drawer'),
+              subtitle: Text('Say hiiii!'),
+            ),
+            RaisedButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text('Close'),
+            )
+          ],
+        ),
+      )),
       body: Container(
         padding: EdgeInsets.all(32),
         child: Center(
