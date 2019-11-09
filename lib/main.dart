3f337deed41993e0b696ed8bda55e65f4c043738
Aditya Chandra <inibukanadit@gmail.com>
2019-11-10 03:57:14 +0700

    complete notify4

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(home: MyApp()));
@@ -9,7 +11,46 @@ class MyApp extends StatefulWidget {
   }
 }
 
+enum Answers { YES, NO, MAYBE }
+
 class _State extends State<MyApp> {
+  String _value = '';
+
+  void _setValue(String value) {
+    setState(() {
+      _value = value;
+    });
+  }
+
+  Future _askUser() async {
+    switch (await showDialog(
+        context: context,
+        child: SimpleDialog(
+          title: Text('Do you like Flutter?'),
+          children: <Widget>[
+            SimpleDialogOption(
+                child: Text('Yes!!'),
+                onPressed: () => Navigator.pop(context, Answers.YES)),
+            SimpleDialogOption(
+                child: Text('NO :('),
+                onPressed: () => Navigator.pop(context, Answers.NO)),
+            SimpleDialogOption(
+                child: Text('Maybe?'),
+                onPressed: () => Navigator.pop(context, Answers.MAYBE)),
+          ],
+        ))) {
+      case Answers.YES:
+        _setValue('Yes');
+        break;
+      case Answers.NO:
+        _setValue('NOOO');
+        break;
+      case Answers.MAYBE:
+        _setValue('Maybe');
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +61,10 @@ class _State extends State<MyApp> {
         padding: EdgeInsets.all(32),
         child: Center(
           child: Column(
-            children: <Widget>[Text('Add Widgets here')],
+            children: <Widget>[
+              Text(_value),
+              RaisedButton(onPressed: _askUser, child: Text('Ask me!'))
+            ],
           ),
         ),
       ),
