cc3af3c1acdbb2ab4cf08cf2da48d16d5eea4455
Aditya Chandra <inibukanadit@gmail.com>
2019-11-10 03:10:28 +0700

    complete scaf2

--- lib/main.dart
+++ lib/main.dart
@@ -10,17 +10,30 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
+  String _value = '';
+
+  void _onClicked() {
+    setState(() {
+      _value = new DateTime.now().toString();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Name Here'),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _onClicked,
+        mini: false,
+        child: Icon(Icons.timer),
+      ),
       body: Container(
         padding: EdgeInsets.all(32),
         child: Center(
           child: Column(
-            children: <Widget>[Text('Add Widgets here')],
+            children: <Widget>[Text(_value)],
           ),
         ),
       ),
