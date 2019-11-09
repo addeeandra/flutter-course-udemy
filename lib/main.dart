02920b8b8d7b27d14e7a4d5e4dccce3646406291
Aditya Chandra <inibukanadit@gmail.com>
2019-11-10 03:48:28 +0700

    complete notify3

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(home: MyApp()));
@@ -10,6 +12,22 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
+  Future _showAlert(BuildContext context, String message) async {
+    return showDialog(
+        context: context,
+        child: AlertDialog(
+          title: Text(message),
+          actions: <Widget>[
+            FlatButton(
+                onPressed: () => Navigator.pop(context),
+                child: Text('YES!')),
+            FlatButton(
+                onPressed: () => Navigator.pop(context),
+                child: Text('OF COURSE', style: TextStyle(color: Colors.red),))
+          ],
+        ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +38,12 @@ class _State extends State<MyApp> {
         padding: EdgeInsets.all(32),
         child: Center(
           child: Column(
-            children: <Widget>[Text('Add Widgets here')],
+            children: <Widget>[
+              Text('Add Widgets here'),
+              RaisedButton(
+                  onPressed: () => _showAlert(context, "Do your like Flutter?"),
+                  child: Text('Show Dialog'))
+            ],
           ),
         ),
       ),
