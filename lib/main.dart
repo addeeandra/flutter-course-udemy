710954ec0b80a2484598a3750f8f71e26b3254cf
Aditya Chandra <inibukanadit@gmail.com>
2019-11-09 22:10:25 +0700

    finish button3

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,14 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
+  String _value = 'Hello world!';
+
+  void _onPressed() {
+    setState(() {
+      _value = 'Yes, you did it!';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +28,10 @@ class _State extends State<MyApp> {
         padding: EdgeInsets.all(32),
         child: Center(
           child: Column(
-            children: <Widget>[Text('Add Widgets here')],
+            children: <Widget>[
+              Text(_value),
+              FlatButton(onPressed: _onPressed, child: Text('Click me!'))
+            ],
           ),
         ),
       ),
