f835cd134bcbfc96f6cc1fbfaac47b987065208a
Aditya Chandra <inibukanadit@gmail.com>
2019-11-10 03:19:07 +0700

    complete scaf4

--- lib/main.dart
+++ lib/main.dart
@@ -10,17 +10,30 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
+  String _value = '';
+
+  void _onClick(String value) {
+    setState(() {
+      _value = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Name Here'),
       ),
+      persistentFooterButtons: <Widget>[
+        IconButton(icon: Icon(Icons.timer), onPressed: () => _onClick('Button 1')),
+        IconButton(icon: Icon(Icons.home), onPressed: () => _onClick('Button 2')),
+        IconButton(icon: Icon(Icons.adb), onPressed: () => _onClick('Button 3')),
+      ],
       body: Container(
         padding: EdgeInsets.all(32),
         child: Center(
           child: Column(
-            children: <Widget>[Text('Add Widgets here')],
+            children: <Widget>[Text(_value)],
           ),
         ),
       ),
