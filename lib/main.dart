64f86cc3b785199bcc799b12e3dd854df648450c
Aditya Chandra <inibukanadit@gmail.com>
2019-11-10 04:10:07 +0700

    complete assignment 4

--- lib/main.dart
+++ lib/main.dart
@@ -10,17 +10,38 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
+
+  String _value = '';
+
+  void _onPressed() {
+    _value = 'Uh. Oh! I\'m Pressed!';
+    Navigator.pop(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Name Here'),
       ),
+      drawer: Drawer(
+        child: Container(
+          child: Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: Column(
+              children: <Widget>[
+                Text('Here is a text'),
+                FlatButton(onPressed: _onPressed, child: Text('I\'m just a Button'))
+              ],
+            ),
+          ),
+        ),
+      ),
       body: Container(
         padding: EdgeInsets.all(32),
         child: Center(
           child: Column(
-            children: <Widget>[Text('Add Widgets here')],
+            children: <Widget>[Text(_value)],
           ),
         ),
       ),
