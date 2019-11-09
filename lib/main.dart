0d2c81e9dae81fc2d7af2cfb2fa4453ab4e1716b
Aditya Chandra <inibukanadit@gmail.com>
2019-11-10 03:42:43 +0700

    notify2-snackbar

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,20 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
+  final GlobalKey<ScaffoldState> _scaffoldState = GlobalKey<ScaffoldState>();
+
+  void _showbar() {
+    _scaffoldState.currentState.showSnackBar(SnackBar(
+      content: Text('Hello world!'),
+      backgroundColor: Colors.blue,
+      duration: Duration(seconds: 2),
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldState,
       appBar: AppBar(
         title: Text('Name Here'),
       ),
@@ -20,7 +31,10 @@ class _State extends State<MyApp> {
         padding: EdgeInsets.all(32),
         child: Center(
           child: Column(
-            children: <Widget>[Text('Add Widgets here')],
+            children: <Widget>[
+              Text('Add Widgets here'),
+              RaisedButton(onPressed: _showbar, child: Text('Click me!'))
+            ],
           ),
         ),
       ),
