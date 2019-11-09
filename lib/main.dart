39465dd1fb4b2e65641ce66caa1560457fa90dc4
Aditya Chandra <inibukanadit@gmail.com>
2019-11-10 03:37:12 +0700

    complete notify1

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,29 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
+  void _showBottom() {
+    showModalBottomSheet<void>(
+        context: context,
+        builder: (BuildContext context) {
+          return Container(
+              padding: EdgeInsets.all(16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Some information here',
+                    style: TextStyle(
+                        color: Colors.red, fontWeight: FontWeight.bold),
+                  ),
+                  RaisedButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: Text('Close'),
+                  )
+                ],
+              ));
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +43,13 @@ class _State extends State<MyApp> {
         padding: EdgeInsets.all(32),
         child: Center(
           child: Column(
-            children: <Widget>[Text('Add Widgets here')],
+            children: <Widget>[
+              Text('Add Widgets here'),
+              RaisedButton(
+                onPressed: _showBottom,
+                child: Text('Click me'),
+              )
+            ],
           ),
         ),
       ),
