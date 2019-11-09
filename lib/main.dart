a162cb7f31cb4b8e25ed66eb8c312de26bf16286
Aditya Chandra <inibukanadit@gmail.com>
2019-11-09 22:23:42 +0700

    complete input2

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,21 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
+  bool _value1 = false;
+  bool _value2 = false;
+
+  void _onValue1Changed(bool value) {
+    setState(() {
+      _value1 = value;
+    });
+  }
+
+  void _onValue2Changed(bool value) {
+    setState(() {
+      _value2 = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +35,18 @@ class _State extends State<MyApp> {
         padding: EdgeInsets.all(32),
         child: Center(
           child: Column(
-            children: <Widget>[Text('Add Widgets here')],
+            children: <Widget>[
+              Text('Checkbox Example'),
+              Checkbox(value: _value1, onChanged: _onValue1Changed),
+              CheckboxListTile(
+                value: _value2,
+                onChanged: _onValue2Changed,
+                title: Text('Agree with our terms'),
+                subtitle: Text('See our terms & condition'),
+                controlAffinity: ListTileControlAffinity.leading,
+                activeColor: Colors.red,
+              )
+            ],
           ),
         ),
       ),
