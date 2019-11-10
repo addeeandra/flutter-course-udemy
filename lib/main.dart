fb536db73a92b4bdcda81b001254a5815abc45a1
Aditya Chandra <inibukanadit@gmail.com>
2019-11-10 15:14:37 +0700

    complete layout4

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(MaterialApp(home: MyApp()));
 
@@ -10,6 +13,26 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
+  Map _countries = Map();
+
+  void _getData() async {
+    var url = 'http://country.io/names.json';
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      setState(() {
+        _countries = jsonDecode(response.body);
+        print('Loaded ${_countries.length} countries');
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +43,21 @@ class _State extends State<MyApp> {
         padding: EdgeInsets.all(32),
         child: Center(
           child: Column(
-            children: <Widget>[Text('Add Widgets here')],
+            children: <Widget>[
+              Text('Countries', style: TextStyle(fontWeight: FontWeight.bold)),
+              Expanded(child: ListView.builder(
+                itemCount: _countries.length,
+                itemBuilder: (BuildContext context, int index) {
+                  String _key = _countries.keys.elementAt(index);
+                  return Row(
+                    children: <Widget>[
+                      Text('$_key : '),
+                      Text(_countries[_key])
+                    ],
+                  );
+                },
+              ))
+            ],
           ),
         ),
       ),
