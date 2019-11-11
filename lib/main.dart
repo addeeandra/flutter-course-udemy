5396438372a6b36c17c9b06e528ff3fd7b5756d8
Aditya Chandra <inibukanadit@gmail.com>
2019-11-11 14:00:35 +0700

    complete intermediate-2-1

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,43 @@ class MyApp extends StatefulWidget {
   }
 }
 
+class MyItem {
+  bool isExpanded;
+  final String header;
+  final Widget body;
+
+  MyItem(this.isExpanded, this.header, this.body);
+}
+
 class _State extends State<MyApp> {
+  List<MyItem> _items = List<MyItem>();
+
+  @override
+  void initState() {
+    for (int i = 0; i < 10; i++) {
+      _items.add(MyItem(
+          false,
+          'Item in $i',
+          Container(
+            padding: EdgeInsets.all(16),
+            child: Text('Hello World!'),
+          )));
+    }
+    super.initState();
+  }
+
+  ExpansionPanel _createItem(MyItem item) {
+    return ExpansionPanel(
+        headerBuilder: (BuildContext context, bool isExpanded) {
+          return Container(
+            padding: EdgeInsets.all(8),
+            child: Text('Header ${item.header}'),
+          );
+        },
+        body: item.body,
+        isExpanded: item.isExpanded);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,10 +54,17 @@ class _State extends State<MyApp> {
       ),
       body: Container(
         padding: EdgeInsets.all(32),
-        child: Center(
-          child: Column(
-            children: <Widget>[Text('Add Widgets here')],
-          ),
+        child: ListView(
+          children: <Widget>[
+            ExpansionPanelList(
+              expansionCallback: (int index, bool isExpanded) {
+                setState(() {
+                  _items[index].isExpanded = !_items[index].isExpanded;
+                });
+              },
+              children: _items.map(_createItem).toList(),
+            ),
+          ],
         ),
       ),
     );
