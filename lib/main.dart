515a2018abbfa2af3b524ba6d64015fe3ffa1746
Aditya Chandra <inibukanadit@gmail.com>
2019-11-11 13:31:26 +0700

    finish intermediate-1-4

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,37 @@ class MyApp extends StatefulWidget {
   }
 }
 
+enum Animals { Cat, Dog, Bird, Lizard, Fish }
+
 class _State extends State<MyApp> {
+  Animals _selected = Animals.Cat;
+  String _value = 'Make a Selection';
+  List<PopupMenuItem<Animals>> _items = new List();
+
+  @override
+  void initState() {
+    for (Animals animal in Animals.values) {
+      _items.add(PopupMenuItem(
+        child: Text(_getDisplay(animal)),
+        value: animal,
+      ));
+    }
+    super.initState();
+  }
+
+  void _onSelected(Animals animal) {
+    setState(() {
+      _selected = animal;
+      _value = 'You Selected ${_getDisplay(animal)}';
+    });
+  }
+
+  String _getDisplay(Animals animal) {
+    int index = animal.toString().indexOf('.');
+    index++;
+    return animal.toString().substring(index);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,8 +49,21 @@ class _State extends State<MyApp> {
       body: Container(
         padding: EdgeInsets.all(32),
         child: Center(
-          child: Column(
-            children: <Widget>[Text('Add Widgets here')],
+          child: Row(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(16.0),
+                child: Text(_value),
+              ),
+              PopupMenuButton<Animals>(
+                child: Icon(Icons.input),
+                initialValue: Animals.Cat,
+                onSelected: _onSelected,
+                itemBuilder: (BuildContext context) {
+                  return _items;
+                }
+              )
+            ],
           ),
         ),
       ),
